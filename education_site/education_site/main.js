ca10050508bd8d226145f2325b016a10b02e00e1
Param-khandwal <124156126+Param-khandwal@users.noreply.github.com>
2023-09-06 11:15:49 +0530

    Add files via upload

---
+++ education_site/education_site/main.js
@@ -0,0 +1,50 @@
+
+//  change navbar  stylle on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle
+        ('window-scroll', window.scrollY > 0)
+})
+
+// show/hide faq answer 
+
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        const icon = faq.querySelector('.faq_icon i ');
+        if (icon.className === 'uil uil-plus') {
+            icon.className = "uil uil-minus";
+        }
+        else {
+            icon.className = "uil uil-plus";
+        }
+    })
+})
+
+//  show /hode nav menu
+
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closebtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click',()=>{
+    menu.style.display="flex";
+    closebtn.style.display="inline-block";
+    menuBtn.style.display="none";
+})
+
+
+// close nav men u
+
+const closeNav=() => {
+    menu.style.display="none";
+    closebtn.style.display="none";
+    menuBtn.style.display="inline-block";
+}
+
+closebtn.addEventListener('click',closeNav)
+
+
+
